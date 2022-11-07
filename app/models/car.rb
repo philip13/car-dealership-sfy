4c324228d70197fe871181a2265c1b18314f6a9a
Philip <fili.cueavas@gmail.com>
2022-11-07 13:22:50 -0600

    app/models/car.rb app/models/dealership.rb include relationship has_many_belongs_to

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,4 @@
 class Car < ApplicationRecord
+  has_and_belongs_to_many :dealerships
   validates :brand, :price, :condition, :entry_date, presence: true
 end
--- app/models/dealership.rb
+++ app/models/dealership.rb
@@ -1,3 +1,4 @@
 class Dealership < ApplicationRecord
+  has_and_belongs_to_many :cars
   validates :name, :address, :country, presence: true
 end

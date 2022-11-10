1be898e6c61214dd0b6c35f4367134d986ef21ed
Philip <fili.cueavas@gmail.com>
2022-11-10 01:20:26 -0600

    db/seeds.rb include new method to create Car and Dealerships and relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+dealerships = []
+10.times do
+  dealerships << Dealership.create(
+    name: Faker::Company.name, 
+    address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    country: Faker::Address.state
+    )
+end
+
+puts "dealerships created = #{dealerships.inspect}"
+
+10.times do |i|
+  v_make = Faker::Vehicle.make
+  
+  car = Car.create(
+    brand: v_make, 
+    model: Faker::Vehicle.model(make_of_model: v_make),
+    sub_model: Faker::Vehicle.year,
+    price: Faker::Number.between(from: 500, to: 100000.01).round(2),
+    condition: 'Used', 
+    entry_date: Faker::Date.between(from: 4.years.ago, to: Date.today)
+  )
+  c_dealer = CarsDealership.create(car_id: car.id, dealership_id: dealerships[i].id)
+end
+
+10.times do |i|
+  v_make = Faker::Vehicle.make
+  
+  car = Car.create(
+    brand: v_make, 
+    model: Faker::Vehicle.model(make_of_model: v_make),
+    sub_model: Faker::Vehicle.year,
+    price: Faker::Number.between(from: 500, to: 100000.01).round(2),
+    condition: 'New', 
+    entry_date: Faker::Date.between(from: 1.years.ago, to: Date.today)
+  )
+
+  c_dealer = CarsDealership.create(car_id: car.id, dealership_id: dealerships[i].id)
+end

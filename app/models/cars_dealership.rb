9723b94dcbc3b42295ffe4f6927d4e2faf79b83e
Philip <fili.cueavas@gmail.com>
2022-11-10 00:56:35 -0600

    app/models/cars_dealership.rb new validation on car can only be located at one place at the same time

--- app/models/cars_dealership.rb
+++ app/models/cars_dealership.rb
@@ -1,4 +1,18 @@
 class CarsDealership < ApplicationRecord
   belongs_to :car
   belongs_to :dealership
+
+  validate :car_located_at_unique_dealership
+
+  def car_located_at_unique_dealership
+    if located_at == true
+      other_dealerships =   CarsDealership.where("car_id = ? AND located_at = ?", car_id, true)
+      other_dealerships.select {|cdealer| cdealer.id != id } unless id.nil?
+
+      if !other_dealerships.empty?
+        errors.add(:located_at, 'allready have Located dealership')
+      end
+    end
+
+  end
 end
